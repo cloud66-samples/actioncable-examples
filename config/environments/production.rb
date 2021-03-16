b570bb14c1c0f127f669b107db5fb94c180f30f9
Dima <dima@cloud66.com>
2021-03-16 13:36:13 +0000

    Add nil too

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
 
   # Set Action Cable server url for consumer connection
   # config.action_cable.url = 'ws://cable.example.com:28080'
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /ws:\/\/*/, /wss:\/\/*/]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /ws:\/\/*/, /wss:\/\/*/, nil]
 end
